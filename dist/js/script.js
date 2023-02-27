9a318f0c9538888524501adfd3e5b3638b2b8f9e
Oksana Bashlachova <ookb2019@gmail.com>
2023-02-28 00:46:37 +0200

    Add the Cart function

--- dist/js/script.js
+++ dist/js/script.js
@@ -313,6 +313,42 @@
 
   }
 
+  class Cart {
+    constructor(element){
+      const thisCart = this;
+
+      thisCart.products = [];
+
+      thisCart.getElements(element);
+      thisCart.initActions();
+      console.log('new Cart:', thisCart);
+    }
+
+    getElements(element){
+      const thisCart = this;
+
+      thisCart.dom = {};
+
+      thisCart.dom.wrapper = element;
+      thisCart.dom.toggleTrigger = element.querySelector(select.cart.toggleTrigger);
+
+    }
+
+    initActions(){
+      const thisCart = this;
+
+      thisCart.dom.toggleTrigger.addEventListener('click', function(event){
+        event.preventDefault();
+
+        thisCart.dom.wrapper.classList.toggle(classNames.cart.wrapperActive);
+      });
+
+
+
+
+    }
+  }
+
   const app = {
     initData: function(){
       const thisApp = this;
@@ -340,11 +376,20 @@
 
       thisApp.initData();
       thisApp.initMenu();
+      thisApp.initCart();
     },
+
+    initCart:function(){
+      const thisApp = this;
+
+      const cartElem = document.querySelector(select.containerOf.cart);
+      thisApp.cart = new Cart(cartElem);
+    },
+
   };
 
 
-
   app.init();
+
 }
 
